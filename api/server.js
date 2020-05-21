59c98e2bf7b6367db8d3c8d20ce54bf46cff8581
Javier Umana <javierumanafs@gmail.com>
2020-05-21 18:20:53 -0400

    testing

--- api/server.js
+++ api/server.js
@@ -1,27 +1,27 @@
 const express = require("express");
 
-const Projects = require("../projects/projectsModel.js");
+// const Projects = require("../projects/projectsModel.js");
 
-const projectsRouter = require("../projects/projects-router.js");
+// const projectsRouter = require("../projects/projects-router.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/projects', projectsRouter);
+// server.use('/api/projects', projectsRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "IS UP" });
 });
 
-server.get("/projects", (req, res) => {
-  Projects.getAll()
-  .then(projects => {
-    res.status(200).json({ data: projects })
-  })
-  .catch(error => {
-    res.status(500).json(error);
-  });
-});
+// server.get("/projects", (req, res) => {
+//   Projects.getAll()
+//   .then(projects => {
+//     res.status(200).json({ data: projects })
+//   })
+//   .catch(error => {
+//     res.status(500).json(error);
+//   });
+// });
 
 module.exports = server;
